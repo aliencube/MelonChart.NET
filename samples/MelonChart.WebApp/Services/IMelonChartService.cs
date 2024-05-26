fba59cb9bcd1ba9bfa35600fe5f2a0c8e69d43d8
Justin Yoo <justin.yoojh@gmail.com>
2024-05-26 18:55:39 +0900

    Add web app sample

---
+++ samples/MelonChart.WebApp/Services/IMelonChartService.cs
@@ -0,0 +1,16 @@
+﻿using MelonChart.Models;
+
+namespace MelonChart.WebApp.Services;
+
+/// <summary>
+/// This provides interfaces to the <see cref="MelonChartService"/> class.
+/// </summary>
+public interface IMelonChartService
+{
+    /// <summary>
+    /// Runs the application asynchronously.
+    /// </summary>
+    /// <param name="chartType"><see cref="ChartTypes"/> value.</param>
+    /// <returns>Returns the <see cref="ChartItemCollection"/> instance.</returns>
+    Task<ChartItemCollection> RunAsync(ChartTypes? chartType = ChartTypes.Top100);
+}
